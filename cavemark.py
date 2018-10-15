8b48a9617d4bb1346992274ec178a9b3cb75e60c
caveman <toraboracaveman@protonmail.com>
2018-10-15 21:01:53 +0400

    k

--- cavemark.py
+++ cavemark.py
@@ -306,11 +306,11 @@ class CaveMark:
 
         # emphasized text format
         if frmt_emph_prefix is None:
-            self.frmt_emph_prefix = '<em>&ldquo;'
+            self.frmt_emph_prefix = '<em>'
         else:
             self.frmt_emph_prefix = frmt_emph_prefix
         if frmt_emph_suffix is None:
-            self.frmt_emph_suffix = '&rdquo;</em>'
+            self.frmt_emph_suffix = '</em>'
         else:
             self.frmt_emph_suffix = frmt_emph_suffix
 
